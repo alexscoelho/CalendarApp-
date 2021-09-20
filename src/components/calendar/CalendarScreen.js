b376eb4a8218e63548bfb4cfc6e27e89a325ebf8
Alexson Coelho <coelho@Alexsons-MacBook-Pro.local>
2021-09-19 21:30:39 -0400

    styling calendar

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,10 +1,51 @@
 import React from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "moment/locale/es";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
 import { Navbar } from "../ui/Navbar";
+import { messages } from "../../helpers/calendar-messages-es";
+
+const localizer = momentLocalizer(moment);
+moment.locale("es");
+
+const events = [
+  {
+    title: "Cumple años del jefe",
+    start: moment().toDate(),
+    end: moment().add(2, "hours").toDate(),
+    bgcolor: "#fafafa",
+    notes: "comprar pastel",
+  },
+];
 
 export const CalendarScreen = () => {
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    const style = {
+      backgroundColor: "#367CF7",
+      borderRadius: "0px",
+      opacity: 0.8,
+      display: "block",
+      color: "white",
+    };
+    return {
+      style,
+    };
+  };
   return (
-    <div>
+    <div className='calendar-screen'>
       <Navbar />
+
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor='start'
+        endAccessor='end'
+        style={{ height: 500 }}
+        messages={messages}
+        eventPropGetter={eventStyleGetter}
+      />
     </div>
   );
 };
