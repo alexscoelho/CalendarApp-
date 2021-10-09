aea055ffe55cd0abf7f3ab0c9dfa32051f00245b
Alexson Coelho <coelho@Alexsons-MacBook-Pro.local>
2021-10-08 20:59:08 -0400

    login and register

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,26 +1,41 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import { startLogin } from "../../actions/auth";
+import Swal from "sweetalert2";
+import { startLogin, startRegister } from "../../actions/auth";
 import { useForm } from "../../hooks/useForm";
 import "./login.css";
 
 export const LoginScreen = () => {
   const dispatch = useDispatch();
-  const initialForm = {
-    email: "",
-    password: "",
-  };
 
   const [formLoginValues, handleLoginInputChange] = useForm({
-    lEmail: "",
-    lPassword: "",
+    lEmail: "alexson@hotmail.com",
+    lPassword: "123456",
   });
 
   const { lEmail, lPassword } = formLoginValues;
 
+  const [formRegisterValues, handleRegisterInputChange] = useForm({
+    rName: "Alba",
+    rEmail: "alba@hotmail.com",
+    rPassword1: "123456",
+    rPassword2: "123456",
+  });
+
+  const { rEmail, rPassword1, rPassword2, rName } = formRegisterValues;
+
   const handleLogin = (e) => {
     e.preventDefault();
     dispatch(startLogin(lEmail, lPassword));
+  };
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+
+    if (rPassword1 !== rPassword2) {
+      return Swal.fire("Error", "Las contraseñas deben ser iguales", "error");
+    }
+    dispatch(startRegister(rEmail, rPassword1, rName));
   };
 
   return (
@@ -57,12 +72,15 @@ export const LoginScreen = () => {
 
         <div className='col-md-6 login-form-2'>
           <h3>Registro</h3>
-          <form>
+          <form onSubmit={handleRegister}>
             <div className='form-group'>
               <input
                 type='text'
                 className='form-control'
                 placeholder='Nombre'
+                name='rName'
+                value={rName}
+                onChange={handleRegisterInputChange}
               />
             </div>
             <div className='form-group'>
@@ -70,6 +88,9 @@ export const LoginScreen = () => {
                 type='email'
                 className='form-control'
                 placeholder='Correo'
+                name='rEmail'
+                value={rEmail}
+                onChange={handleRegisterInputChange}
               />
             </div>
             <div className='form-group'>
@@ -77,6 +98,9 @@ export const LoginScreen = () => {
                 type='password'
                 className='form-control'
                 placeholder='Contraseña'
+                name='rPassword1'
+                value={rPassword1}
+                onChange={handleRegisterInputChange}
               />
             </div>
 
@@ -85,6 +109,9 @@ export const LoginScreen = () => {
                 type='password'
                 className='form-control'
                 placeholder='Repita la contraseña'
+                name='rPassword2'
+                value={rPassword2}
+                onChange={handleRegisterInputChange}
               />
             </div>
 
